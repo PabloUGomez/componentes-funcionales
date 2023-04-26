8452542f3f4bd4540bb599817d418928fb34fec8
Pablo Gomez <pablogomez0430@gmail.com>
2023-04-26 04:59:04 -0300

    Form finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import FormSingUp from './componentes/FormSingUp';
+import { Container,Typography } from '@mui/material';
 
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log(valores);
+  }
+
   return (
     <>
-      <h1>Formulario registro</h1>
-      <FormSingUp/>
+      <Container component="section" maxWidth="sm">
+        <Typography variant='h3' align='center'>Formulario registro</Typography>
+        <FormSingUp handleSubmit={handleSubmit}/>
+      </Container>
     </>
   );
 }
--- src/componentes/FormSingUp.jsx
+++ src/componentes/FormSingUp.jsx
@@ -1,7 +1,119 @@
-function FormSingUp() {
-    return <form>
-        <input type="text"></input>
-        <button> Registrarse</button>
+import { useState } from 'react';
+import { 
+    TextField,
+    Switch,
+    Button,
+    FormGroup,
+    FormControlLabel
+} from '@mui/material';
+
+function FormSingUp(props) {
+
+    const {handleSubmit} = props;
+    const [name,setName] = useState('');
+    const [apellido,setApellido] = useState('');
+    const [email,setEmail] = useState('');
+    const [promocion,setPromocion] = useState(true);
+    const [notificacion,setNotificacion] = useState(false);
+
+    const [errors,setErrors] = useState({
+        name: {
+            error: false,
+            messase: "Deben ser al menos 3 caracteres"
+        }
+    })
+    
+    function validarNombre(nombre){
+        if (nombre.length >= 3) {
+            return {name: {
+                error: false,
+                messase: ""
+            }}
+        }else{
+            return {name: {
+                error: true,
+                messase: "Deben ser al menos 3 caracteres"
+            }}
+        }
+    }
+
+    return <form 
+            onSubmit={(e) => {
+                e.preventDefault()
+                handleSubmit({
+                    name,
+                    apellido,
+                    email,
+                    promocion,
+                    notificacion
+                })
+            }}>
+        <TextField 
+            id='name' 
+            label="Nombre" 
+            variant="outlined"
+            fullWidth
+            required
+            margin='normal'
+            onChange={(e) => {
+                setName(e.target.value);
+            }}
+            error = {errors.name.error}
+            helperText= {errors.name.error ? errors.name.messase : ""}
+            onBlur={(e) => {
+                setErrors(
+                    validarNombre(e.target.value)
+                )
+            }}
+        />
+        <TextField 
+            id='apellido' 
+            label="Apellido" 
+            variant="outlined"
+            fullWidth
+            required
+            margin='normal'
+            onChange={(e) => {
+                setApellido(e.target.value);
+            }}
+        />
+        <TextField 
+            id='email' 
+            label="Email" 
+            type='email'
+            variant="outlined"
+            fullWidth
+            required
+            margin='normal'
+            onChange={(e) => {
+                setEmail(e.target.value);
+            }}
+        />
+        <FormGroup>
+            <FormControlLabel 
+                control={
+                    <Switch 
+                        checked={promocion}
+                        onChange={(e) => {
+                            setPromocion(e.target.checked);
+                        }}
+                    />
+                } 
+                label="Promociones"
+            />
+            <FormControlLabel 
+                control={
+                    <Switch 
+                        checked={notificacion}
+                        onChange={(e) => {
+                            setNotificacion(e.target.checked);
+                        }}
+                    />
+                } 
+                label="Notificaciones"
+            />
+        </FormGroup>
+        <Button variant='contained' type="submit"> Registrarse</Button>
     </form>;
 }
 
